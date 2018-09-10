5dfcf79904f83c8f04b010b2b6a3398d3a75c2dc
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2018-09-10 17:03:55 +0530

    Basic File Structure

---
+++ classes/student.py
@@ -0,0 +1,10 @@
+class Student :
+
+    def __init__(self,id,name,school,marks,lci):
+        self.id = id
+        self.name = name
+        self.school = school
+        self.marks = marks
+        self.lci = lci
+
+
